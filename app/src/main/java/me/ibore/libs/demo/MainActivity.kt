66f8f611d3d860f10fa45b799436b5387533a984
Ibore Xie <bore521@163.com>
2018-01-24 20:16:56 +0800

    0124

--- app/src/main/java/me/ibore/libs/demo/MainActivity.kt
+++ app/src/main/java/me/ibore/libs/demo/MainActivity.kt
@@ -1,10 +1,8 @@
 package me.ibore.libs.demo
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
-import me.ibore.video.CustomerController
-import me.ibore.video.XVideoPlayer.TYPE_IJK
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,18 +14,25 @@ class MainActivity : AppCompatActivity() {
 //        var transformation = RoundedCornersTransformation(10, 0)
 //        var transformation = RoundedCornersTransformation(10, 0)
 //        Glide.with(roundImageView).load(R.mipmap.head).apply(RequestOptions.bitmapTransform(transformation)).into(roundImageView)
+//        val imagePicker = ImagePicker.getInstance()
+//        imagePicker.imageLoader = ImageLoader()   //设置图片加载器
+//        imagePicker.isShowCamera = true                      //显示拍照按钮
+//        imagePicker.isCrop = true                           //允许裁剪（单选才有效）
+//        imagePicker.isSaveRectangle = true                   //是否按矩形区域保存
+//        imagePicker.selectLimit = 1              //选中数量限制
+//        imagePicker.style = CropImageView.Style.RECTANGLE  //裁剪框的形状
+//        imagePicker.focusWidth = 800                       //裁剪框的宽度。单位像素（圆形自动取宽高最小值）
+//        imagePicker.focusHeight = 800                      //裁剪框的高度。单位像素（圆形自动取宽高最小值）
+//        imagePicker.outPutX = 1000                         //保存文件的宽度。单位像素
+//        imagePicker.outPutY = 1000                         //保存文件的高度。单位像素
+//
+//
+//        ImagePicker.getInstance().selectLimit = 1
+//        val intent = Intent(this, ImageGridActivity::class.java)
+//        intent.putExtra(ImageGridActivity.EXTRAS_TAKE_PICKERS, true) // 是否是直接打开相机
+//        startActivityForResult(intent, 1)
 
-        val controller = CustomerController(this)
 
-        xVideoPlayer.setController(controller)
-        xVideoPlayer.setPlayerType(TYPE_IJK)
-        controller.setTitle("测试标题")
-        controller.setLenght(25000)
-
-        xVideoPlayer.setUp("http://tanzi27niu.cdsb.mobi/wps/wp-content/uploads/2017/04/2017-04-28_18-20-56.mp4", null)
-
-        val okhttp = OkHttpClient()
-        okhttp.
     }
 }
 
--- libs/src/main/java/me/ibore/libs/audio/AudioRecord.java
+++ libs/src/main/java/me/ibore/libs/audio/AudioRecord.java
@@ -6,6 +6,8 @@ package me.ibore.libs.audio;
 
 import android.media.AudioManager;
 import android.media.MediaRecorder;
+import android.os.Handler;
+import android.support.annotation.UiThread;
 import android.util.Log;
 
 import java.io.File;
@@ -22,153 +24,97 @@ import java.util.UUID;
  */
 public final class AudioRecord {
 
+    private static final int RECORDING_BITRATE = 32000;
+    private static final int RECORDING_SAMPLING = 16000;
     private MediaRecorder mRecorder;
-    //文件夹位置
-    private String mDirString;
-    //录音文件保存路径
-    private String mCurrentFilePathString;
-    //是否真备好开始录音
-    private boolean isPrepared;
+    private Handler mHandler = new Handler();
+    private Runnable mHandlerRunable;
+    private File mRecordFile;
+    private boolean isRecording = false;
+    private OnRecordListener mListener;
+    private int mMaxVol = 0x7FFF;
+    private int mCount = 0;
 
-    /**
-     * 单例化这个类
-     */
-    private static AudioRecord mInstance;
-
-    private AudioRecord(String dir) {
-        mDirString = dir;
-    }
-
-    public static AudioRecord getInstance(String dir) {
-        if (mInstance == null) {
-            synchronized (AudioManager.class) {
-                if (mInstance == null) {
-                    mInstance = new AudioRecord(dir);
-
-                }
-            }
+    public void startRecord(File recordFile) {
+        if (isRecording()) return;
+        if (null == recordFile) {
+            throw new IllegalStateException("recordFile can't be empty");
         }
-        return mInstance;
-    }
-
-    /**
-     * 回调函数，准备完毕，准备好后，button才会开始显示录音
-     */
-    public interface OnRecordListenter {
-        void wellPrepared();
-    }
-
-    public OnRecordListenter mListener;
-
-    public void setOnAudioStageListener(OnRecordListenter listener) {
-        mListener = listener;
-    }
-
-    // 准备方法
-    public void prepareAudio() {
+        this.mRecordFile = recordFile;
         try {
-            // 一开始应该是false的
-            isPrepared = false;
-            //创建所属文件夹
-            File dir = new File(mDirString);
-            if (!dir.exists()) {
-                dir.mkdirs();
+            if (mRecorder != null) {
+                mRecorder.release();
+                mRecorder = null;
             }
-            String fileNameString = generalFileName();
-            File file = new File(dir, fileNameString);
-            //获取文件
-            mCurrentFilePathString = file.getAbsolutePath();
-
             mRecorder = new MediaRecorder();
-            // 设置输出文件
-            mRecorder.setOutputFile(file.getAbsolutePath());
-            // 设置meidaRecorder的音频源是麦克风
             mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-            // 设置文件音频的输出格式为amr
-            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
-            // 设置音频的编码格式为amr。这里采用AAC主要为了适配IOS，保证在IOS上可以正常播放。
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.AAC_ADTS);
             mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
-
-            // 严格遵守google官方api给出的mediaRecorder的状态流程图
+            mRecorder.setAudioChannels(1);
+            mRecorder.setAudioSamplingRate(RECORDING_SAMPLING);
+            mRecorder.setAudioEncodingBitRate(RECORDING_BITRATE);
+            mRecorder.setOutputFile(mRecordFile.getAbsolutePath());
             mRecorder.prepare();
-
+            isRecording = true;
             mRecorder.start();
-            // 准备结束
-            isPrepared = true;
-            // 已经准备好了，可以录制了
-            if (mListener != null) {
-                mListener.wellPrepared();
-            }
-
-        } catch (IllegalStateException e) {
-            e.printStackTrace();
+            initTimer();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
-    /**
-     * 随机生成文件的名称
-     *
-     * @return
-     */
-    private String generalFileName() {
-        return UUID.randomUUID().toString() + ".amr";
-    }
-
-    // 获得声音的level
-    public int getVoiceLevel(int maxLevel) {
-        // mRecorder.getMaxAmplitude()这个是音频的振幅范围，值域是1-32767
-        if (isPrepared) {
-            try {
-                // 取证+1，否则去不到7
-                return maxLevel * mRecorder.getMaxAmplitude() / 32768 + 1;
-            } catch (Exception e) {
-
+    private void initTimer() {
+        mHandlerRunable = new Runnable() {
+            @Override
+            public void run() {
+                if (mListener != null){
+                    mListener.onRecord(isRecording()? mRecorder.getMaxAmplitude() * mMaxVol / 0x7FFF : 0, (mCount++) * 100);
+                    mHandler.postDelayed(mHandlerRunable, 100);
+                }
             }
-        }
-        return 1;
+        };
+        mHandler.postDelayed(mHandlerRunable, 100);
     }
 
-    // 释放资源
-    public void release() {
-        // 严格按照api流程进行
-        if (mRecorder == null) return;
-        /*
-        * 这里处理一些特定情况下的异常。2017/04/12 by wgyscsf
-         */
-        try {
-            //下面三个参数必须加，不加的话会奔溃，在mediarecorder.stop();
-            //报错为：RuntimeException:stop failed
-            mRecorder.setOnErrorListener(null);
-            mRecorder.setOnInfoListener(null);
-            mRecorder.setPreviewDisplay(null);
-            mRecorder.stop();
-        } catch (IllegalStateException e) {
-            Log.i("Exception", Log.getStackTraceString(e) + "123");
-        } catch (RuntimeException e) {
-            Log.i("Exception", Log.getStackTraceString(e) + "123");
-        } catch (Exception e) {
-            Log.i("Exception", Log.getStackTraceString(e) + "123");
-        }
+    public File stopRecord() {
+        mRecorder.stop();
         mRecorder.release();
         mRecorder = null;
-
+        isRecording = false;
+        mCount = 0;
+        mHandler.removeCallbacks(mHandlerRunable);
+        return mRecordFile;
     }
 
-    // 取消,因为prepare时产生了一个文件，所以cancel方法应该要删除这个文件，
-    // 这是与release的方法的区别
-    public void cancel() {
-        release();
-        if (mCurrentFilePathString != null) {
-            File file = new File(mCurrentFilePathString);
-            file.delete();
-            mCurrentFilePathString = null;
+    public void discardRecord() {
+        mRecorder.stop();
+        mRecorder.release();
+        if (mRecordFile != null && mRecordFile.exists() && mRecordFile.delete()) {
+            isRecording = false;
         }
+        mCount = 0;
+        mHandler.removeCallbacks(mHandlerRunable);
     }
 
-    public String getCurrentFilePath() {
-        return mCurrentFilePathString;
+    public File getRecordFile() {
+        return mRecordFile;
+    }
+
+    public boolean isRecording() {
+        return mRecorder != null && isRecording;
+    }
+
+    public void setOnRecordListener(OnRecordListener listener) {
+        this.mListener = listener;
+    }
+
+
+    public void setMaxVol(int mMaxVol) {
+        this.mMaxVol = mMaxVol;
+    }
+
+    public interface OnRecordListener {
+        @UiThread
+        void onRecord(int amplitude, int recordTime);
     }
 }
