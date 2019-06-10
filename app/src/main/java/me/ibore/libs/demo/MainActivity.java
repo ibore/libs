5b663ef19d7ccd6d78e4667f217c68a4dc7f0f7b
Ibore Xie <bore521@live.com>
2019-06-10 14:04:41 +0800

    0610

--- app/src/main/java/me/ibore/libs/demo/MainActivity.java
+++ app/src/main/java/me/ibore/libs/demo/MainActivity.java
@@ -6,8 +6,6 @@ import androidx.annotation.Nullable;
 
 import me.ibore.libs.basic.XActivity;
 import me.ibore.libs.demo.fm.HomeFragment;
-import me.ibore.libs.dialog.XAlertDialog;
-import me.ibore.libs.http.XHttp;
 import me.ibore.libs.rxbus.Subscribe;
 import me.ibore.libs.rxbus.ThreadMode;
 import me.ibore.libs.util.BarUtils;
@@ -36,23 +34,7 @@ public class MainActivity extends XActivity {
                 .replace(R.id.fragment, new HomeFragment())
                 .commit();
 
-        XHttp.getClient().newCall().enqueue();
-        /*addDisposable(XHttp.download(
-                "http://shouji.360tpcdn.com/190429/6555c2813e90cb0e18a8cd418852080a/com.qihoo.appstore_300080086.apk",
-                XHttp.getClient(), getCacheDir(), progress -> {
-                    LogUtils.d(progress.percent());
-                }),
-                new HttpObserver<File>() {
-                    @Override
-                    public void onSuccess(File file) {
-                        LogUtils.d(file.getAbsolutePath());
-                    }
 
-                    @Override
-                    public void onError(Exception e) {
-                        e.printStackTrace();
-                    }
-                });*/
     }
 
 
--- libs/src/main/java/me/ibore/libs/basic/XObserver.java
+++ libs/src/main/java/me/ibore/libs/basic/XObserver.java
@@ -3,8 +3,24 @@ package me.ibore.libs.basic;
 import android.content.Context;
 import android.content.DialogInterface;
 
+import com.google.gson.JsonParseException;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.SocketTimeoutException;
+
+import javax.net.ssl.SSLHandshakeException;
+
 import io.reactivex.observers.DisposableObserver;
+import me.ibore.libs.BuildConfig;
+import me.ibore.libs.R;
 import me.ibore.libs.dialog.LoadDialog;
+import me.ibore.libs.exception.ClientException;
+import me.ibore.libs.exception.HttpException;
+import me.ibore.libs.exception.ServerException;
+import me.ibore.libs.util.Utils;
 import me.ibore.widget.LoadLayout;
 
 public abstract class XObserver<T> extends DisposableObserver<T>implements DialogInterface.OnDismissListener {
@@ -35,8 +51,15 @@ public abstract class XObserver<T> extends DisposableObserver<T>implements Dialo
     }
 
     @Override
+    public void onNext(T t) {
+        onSuccess(t);
+    }
+
+    @Override
     public void onError(Throwable e) {
         showError();
+        //如果你想自己处理这些信息，请重写handleExceptions()这个方法
+        handleExceptions(e);
     }
 
     @Override
@@ -69,4 +92,34 @@ public abstract class XObserver<T> extends DisposableObserver<T>implements Dialo
     public void onDismiss(DialogInterface dialog) {
         if (!isDisposed()) dispose();
     }
+
+    protected void handleExceptions(Throwable e) {
+        if (BuildConfig.DEBUG) e.printStackTrace();
+        if (e instanceof retrofit2.HttpException) {
+            retrofit2.HttpException he = (retrofit2.HttpException) e;
+            onError(new HttpException(he.code(), he.message()));
+        } else if (e instanceof ConnectException) {
+            onError(new HttpException(-1, Utils.getApp().getString(R.string.connect_exception)));
+        }  else if (e instanceof SSLHandshakeException) {
+            onError(new HttpException(-1, Utils.getApp().getString(R.string.ssl_handshake_exception)));
+        } else if (e instanceof IOException) {
+            if (e instanceof SocketTimeoutException) {
+                onError(new HttpException(-1, Utils.getApp().getString(R.string.socket_timeout_exception)));
+            } else {
+                onError(new HttpException(-1, Utils.getApp().getString(R.string.io_exception)));
+            }
+        } else if (e instanceof JsonParseException || e instanceof JSONException) {
+            onError(new ClientException(Utils.getApp().getString(R.string.json_parse_exception)));
+        } else if (e instanceof ServerException) {
+            onError((Exception) e);
+        } else if (e instanceof ClientException) {
+            onError((Exception) e);
+        } else {
+            onError(new ClientException(Utils.getApp().getString(R.string.client_exception)));
+        }
+    }
+
+    public abstract void onSuccess(T t);
+
+    public abstract void onError(Exception e);
 }
